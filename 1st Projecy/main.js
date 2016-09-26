21035d93c1edec948c4688d31e4fd440e528334b
Tirth Surti <tirth.surti@gmail.com>
2016-09-26 22:34:46 +0000

    Create that was easy button

---
+++ 1st Projecy/main.js
@@ -0,0 +1,15 @@
+function saythatwaseasy() {
+ var thatwaseasy = new Audio("that_was_easy.mp3");
+ thatwaseasy.play();   
+}
+
+$("#easy").on("click", saythatwaseasy);
+
+
+$(document).keypress(delegateKeypress);
+
+function delegateKeypress(event) {
+    if (event.charCode == 32) {
+    $("#easy").trigger("click")
+  }
+}
